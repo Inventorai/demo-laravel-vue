ebfa28d943011b39a005e77e137a5c26daeaf990
Inventorai <hello@inventorai.co.uk>
2026-06-07 17:26:30 +0100

    Add app logo and use it for the favicons
    - new /images/logo/app_logo.svg: primary-green rounded-square house mark
    - regenerate favicon.png (512) and favicon.ico (16/32/48) from it
    - add an SVG favicon link with the PNG as fallback

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,7 @@
 
         <title data-inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" type="image/svg+xml" href="/images/logo/app_logo.svg">
         <link rel="icon" type="image/png" href="/favicon.png">
 
         <!-- Fonts -->
